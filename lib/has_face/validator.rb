07c6b17ae934cd1e13bcc4fe789170e3ab9552ac
Mario Visic <mario@mariovisic.com>
2011-07-12 22:35:19 +0800

    Bit of a tidy up.

--- lib/has_face/validator.rb
+++ lib/has_face/validator.rb
@@ -13,37 +13,35 @@ module HasFace
       # Skip validation if globally turned off
       return if HasFace.enable_validation == false
 
-      image      = record.send(attr_name)
-      image_path = image.try(:path) if image.respond_to?(:path)
+      image       = record.send(attr_name)
+      @image_path = image.respond_to?(:path) ? image.path : nil
 
       # Skip validation if our image is nil/blank and allow nil/blank is on
       return if (@allow_nil && image.nil?) || (@allow_blank && image.blank?)
 
-      # Add an error if the url is blank
-      return record.errors.add(attr_name, :no_face) if image_path.blank?
+      # Add an error if the image path is blank
+      return record.errors.add(attr_name, :no_face) if @image_path.blank?
 
-      # Get an parse the JSON response
-      params   = { :api_key => HasFace.api_key, :api_secret => HasFace.api_secret, :image => File.new(image_path, 'rb') }
-
-      begin
-        response = RestClient.post(HasFace.detect_url, params)
-      rescue RestClient::Exception => error
-        return handle_request_error(error)
-      end
-
-      json_response = JSON.parse(response.body)
+      # Get the response and parse to JSON
+      raw_response = RestClient.post(HasFace.detect_url, params)
+      response     = JSON.parse(raw_response)
 
       # Error handling for failed responses
-      return handle_api_error(json_response) unless json_response['status'] == 'success'
-
+      return handle_api_error(response) unless response['status'] == 'success'
 
       # Add errors if no tags are present
-      tags = json_response.try(:[], 'photos').try(:first).try(:[], 'tags') || []
+      tags = response.try(:[], 'photos').try(:first).try(:[], 'tags')
       record.errors.add(attr_name, :no_face) if tags.blank?
 
+    rescue RestClient::Exception => error
+      return handle_request_error(error)
     end
 
     protected
+
+    def params
+      { :api_key => HasFace.api_key, :api_secret => HasFace.api_secret, :image => File.new(@image_path, 'r') }
+    end
 
     def handle_api_error(response)
       error_message = "face.com API Error: \"#{response['error_message']}\" Code: #{response['error_code']}"
